7258fc26287d15dfdbc78622f2b2545bfa96cdcd
JFDoUbLe-U <janis.wiesen@fom-net.de>
2023-12-14 23:39:29 +0100

    kk

--- regex.py
+++ regex.py
@@ -1,12 +1,12 @@
 import re
 
-print("without raw string:")
-# path_to_search = "c:\example\task\new"
-target_string = r"c:\example\task\new\exercises\session1"
+# print("without raw string:")
+# # path_to_search = "c:\example\task\new"
+# target_string = r"c:\example\task\new\exercises\session1"
 
-# regex pattern
-pattern = r"^c:\\example\\task\\new"
-# \n and \t has a special meaning in Python
-# Python will treat them differently
-res = re.search(pattern, target_string)
-print(res.group())
+# # regex pattern
+# pattern = r"^c:\\example\\task\\new"
+# # \n and \t has a special meaning in Python
+# # Python will treat them differently
+# res = re.search(pattern, target_string)
+# print(res.group())
